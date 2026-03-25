4f878641d6c7a2bb7c08f17fa0dc0c6dd852d92d
wslrus54 <evseevalek5ander@yandex.ru>
2026-03-25 18:00:00 +0300

    Refactor FileMonitor to depend on abstractions (DIP)

--- include/FileMonitor.h
+++ include/FileMonitor.h
@@ -1,32 +1,33 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <memory>
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include "IFileChecker.h"
+#include "INotifier.h"
 
 class FileMonitor {
 public:
-    FileMonitor();
+    FileMonitor(std::vector<std::shared_ptr<IFileChecker>> checkers,
+                std::shared_ptr<INotifier> notifier);
     ~FileMonitor();
-    void addFile(const std::string& path);
+
+    bool addFile(const std::string& path);
     bool removeFile(const std::string& path);
     void start();
     void stop();
     void listFiles() const;
 
 private:
-    struct FileState {
-        std::string path;
-        bool exists;
-        uintmax_t size;
-    };
-    std::vector<FileState> files_;
+    std::vector<std::string> files_;
+    std::vector<std::shared_ptr<IFileChecker>> checkers_;
+    std::shared_ptr<INotifier> notifier_;
     std::thread worker_;
     std::atomic<bool> running_;
     mutable std::mutex mutex_;
 
     void run();
     void checkFiles();
-    FileState snapshot(const std::string& path) const;
 };
